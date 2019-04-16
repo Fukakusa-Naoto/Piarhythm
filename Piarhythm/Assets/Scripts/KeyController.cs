3f52af99b9bd2759fe92b647d7bfa9699ff8484f
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-04-16 15:42:40 +0900

    黒鍵実装完了

--- Piarhythm/Assets/Scripts/KeyController.cs
+++ Piarhythm/Assets/Scripts/KeyController.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class KeyController : MonoBehaviour
 {
 	private Image m_image;
+	private Color m_startColor;
 
     // Start is called before the first frame update
     void Start()
     {
 		m_image = GetComponent<Image>();
+		m_startColor = m_image.color;
     }
 
     // Update is called once per frame
@@ -27,6 +29,6 @@ public class KeyController : MonoBehaviour
 
 	public void Release()
 	{
-		m_image.color = Color.white;
+		m_image.color = m_startColor;
 	}
 }
--- Piarhythm/Assets/Scripts/KeyboardManager.cs
+++ Piarhythm/Assets/Scripts/KeyboardManager.cs
@@ -29,9 +29,48 @@ public class KeyboardManager : MonoBehaviour
 		noteInfoList = new List<NoteInfo>();
 		m_keys = new Dictionary<int, GameObject>();
 
-		for(int i=0;i<m_keyboard.transform.childCount;++i)
+		for(int i = 0, j = 0; i < m_keyboard.transform.childCount; ++i)
 		{
-			m_keys[i + 21] = m_keyboard.transform.GetChild(i).gameObject;
+			switch(i + 21)
+			{
+				case 22: m_keys[22] = m_keyboard.transform.GetChild(52).gameObject; break;
+				case 25: m_keys[25] = m_keyboard.transform.GetChild(53).gameObject; break;
+				case 27: m_keys[27] = m_keyboard.transform.GetChild(54).gameObject; break;
+				case 30: m_keys[30] = m_keyboard.transform.GetChild(55).gameObject; break;
+				case 32: m_keys[32] = m_keyboard.transform.GetChild(56).gameObject; break;
+				case 34: m_keys[34] = m_keyboard.transform.GetChild(57).gameObject; break;
+				case 37: m_keys[37] = m_keyboard.transform.GetChild(58).gameObject; break;
+				case 39: m_keys[39] = m_keyboard.transform.GetChild(59).gameObject; break;
+				case 42: m_keys[42] = m_keyboard.transform.GetChild(60).gameObject; break;
+				case 44: m_keys[44] = m_keyboard.transform.GetChild(61).gameObject; break;
+				case 46: m_keys[46] = m_keyboard.transform.GetChild(62).gameObject; break;
+				case 49: m_keys[49] = m_keyboard.transform.GetChild(63).gameObject; break;
+				case 51: m_keys[51] = m_keyboard.transform.GetChild(64).gameObject; break;
+				case 54: m_keys[54] = m_keyboard.transform.GetChild(65).gameObject; break;
+				case 56: m_keys[56] = m_keyboard.transform.GetChild(66).gameObject; break;
+				case 58: m_keys[58] = m_keyboard.transform.GetChild(67).gameObject; break;
+				case 61: m_keys[61] = m_keyboard.transform.GetChild(68).gameObject; break;
+				case 63: m_keys[63] = m_keyboard.transform.GetChild(69).gameObject; break;
+				case 66: m_keys[66] = m_keyboard.transform.GetChild(70).gameObject; break;
+				case 68: m_keys[68] = m_keyboard.transform.GetChild(71).gameObject; break;
+				case 70: m_keys[70] = m_keyboard.transform.GetChild(72).gameObject; break;
+				case 73: m_keys[73] = m_keyboard.transform.GetChild(73).gameObject; break;
+				case 75: m_keys[75] = m_keyboard.transform.GetChild(74).gameObject; break;
+				case 78: m_keys[78] = m_keyboard.transform.GetChild(75).gameObject; break;
+				case 80: m_keys[80] = m_keyboard.transform.GetChild(76).gameObject; break;
+				case 82: m_keys[82] = m_keyboard.transform.GetChild(77).gameObject; break;
+				case 85: m_keys[85] = m_keyboard.transform.GetChild(78).gameObject; break;
+				case 87: m_keys[87] = m_keyboard.transform.GetChild(79).gameObject; break;
+				case 90: m_keys[90] = m_keyboard.transform.GetChild(80).gameObject; break;
+				case 92: m_keys[92] = m_keyboard.transform.GetChild(81).gameObject; break;
+				case 94: m_keys[94] = m_keyboard.transform.GetChild(82).gameObject; break;
+				case 97: m_keys[97] = m_keyboard.transform.GetChild(83).gameObject; break;
+				case 99: m_keys[99] = m_keyboard.transform.GetChild(84).gameObject; break;
+				case 102: m_keys[102] = m_keyboard.transform.GetChild(85).gameObject; break;
+				case 104: m_keys[104] = m_keyboard.transform.GetChild(86).gameObject; break;
+				case 106: m_keys[106] = m_keyboard.transform.GetChild(87).gameObject; break;
+				default: m_keys[i + 21] = m_keyboard.transform.GetChild(j++).gameObject; break;
+			}
 		}
 	}
 
