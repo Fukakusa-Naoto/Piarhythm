ed254be21c32ede6ff18ff0c87863d525bebbbfc
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-04-16 16:55:08 +0900

    白鍵の音の再生を実装

--- Piarhythm/Assets/Scripts/KeyController.cs
+++ Piarhythm/Assets/Scripts/KeyController.cs
@@ -7,12 +7,16 @@ public class KeyController : MonoBehaviour
 {
 	private Image m_image;
 	private Color m_startColor;
+	private AudioSource m_audioSource;
+	private bool m_isPress;
 
     // Start is called before the first frame update
     void Start()
     {
 		m_image = GetComponent<Image>();
 		m_startColor = m_image.color;
+		m_audioSource = GetComponent<AudioSource>();
+		m_isPress = false;
     }
 
     // Update is called once per frame
@@ -25,10 +29,14 @@ public class KeyController : MonoBehaviour
 	public void Press()
 	{
 		m_image.color = Color.red;
+		if (!m_isPress) m_isPress = true;
+		else return;
+		if (m_audioSource.clip) m_audioSource.PlayOneShot(m_audioSource.clip);
 	}
 
 	public void Release()
 	{
+		m_isPress = false;
 		m_image.color = m_startColor;
 	}
 }
