475a68293f4ee61ff9dc6882d9112ab75eb7d62a
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-05-07 14:26:30 +0900

    エディタのレイアウトの作成 ボタンからのBGMの読み込みを実装

--- Piarhythm/Assets/Scripts/Edit/EditManager.cs
+++ Piarhythm/Assets/Scripts/Edit/EditManager.cs
@@ -1,43 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using System.Windows.Forms; //OpenFileDialog用に使う
-using System.IO;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
-
-public class test : MonoBehaviour
+public class EditManager : MonoBehaviour
 {
-	private AudioSource audioSource;
+	private AudioSource m_audioSource;
 	private AudioClip m_audioClip = null;
 	private string m_filePuth = null;
+	public Text m_maxTime;
+	public Text m_nowTime;
 
 
 	// Use this for initialization
 	void Start()
 	{
-		audioSource = gameObject.GetComponent<AudioSource>();
+		m_audioSource = gameObject.GetComponent<AudioSource>();
 	}
 
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			if (!audioSource.isPlaying)
-			{
-				audioSource.Play();
-				Debug.Log("name" + m_audioClip.name);
-				Debug.Log("channel ID" + m_audioClip.channels);
-				float[] data = new float[m_audioClip.samples * m_audioClip.channels];
-				m_audioClip.GetData(data, 0);
-				Debug.Log("length Samples" + data.Length);
-				Debug.Log("frequency" + m_audioClip.frequency);
-			}
-		}
+		// 再生時間の更新
+		if(m_audioClip) m_nowTime.text = m_audioSource.time.ToString();
 
 		// コルーチンのスタート
-		if ((m_filePuth != null) && (m_audioClip == null)) Debug.Log(StartCoroutine("Load", m_filePuth));
+		if ((m_filePuth != null) && (m_audioClip == null)) StartCoroutine("Load", m_filePuth);
 	}
 
 	public void OpenExistFile()
@@ -53,6 +42,8 @@ public class test : MonoBehaviour
 
 		//取得したファイル名を代入する
 		m_filePuth = open_file_dialog.FileName;
+
+		if (m_filePuth != null) m_audioClip = m_audioSource.clip = null;
 	}
 
 
@@ -60,8 +51,41 @@ public class test : MonoBehaviour
 	{
 		var www = UnityWebRequestMultimedia.GetAudioClip("file://" + file, AudioType.WAV);
 		yield return www.SendWebRequest();
-		m_audioClip = GetComponent<AudioSource>().clip = DownloadHandlerAudioClip.GetContent(www);
+		m_audioClip = m_audioSource.clip = DownloadHandlerAudioClip.GetContent(www);
 		string[] str = file.Split('\\');
 		m_audioClip.name = str[str.Length - 1];
+		m_maxTime.text = "/\t";
+		m_maxTime.text += m_audioClip.length.ToString();
+	}
+
+
+	public void OnPlayButton()
+	{
+		if (!m_audioSource.isPlaying) m_audioSource.Play();
+	}
+
+
+	public void OnPauseButton()
+	{
+		m_audioSource.Pause();
+	}
+
+
+
+	public void OnStopButton()
+	{
+		if (m_audioSource.isPlaying) m_audioSource.Stop();
+	}
+
+
+	public void OnSaveButton()
+	{
+
+	}
+
+
+	public void OnLoadButton()
+	{
+
 	}
 }
---
+++ Piarhythm/Assets/Shaders/Texture.shader
@@ -0,0 +1,43 @@
+﻿Shader "UI/Texture"
+{
+	Properties
+	{
+		_MainTex("Texture",2D) = "white"{}
+	}
+		SubShader
+	{
+		Pass
+	{
+		Name "Frag"
+
+		Tags
+	{
+		"RenderType" = "Transparent"
+		"Queue" = "Transparent"
+	}
+		LOD 200
+
+		CGPROGRAM
+		#pragma fragment frag
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+		};
+
+
+		float4 frag(Input IN)
+		{
+			return float4(1, 1, 1, 1);
+			//return tex2D(_MainTex, IN.uv_MainTex);
+		}
+		ENDCG
+	}
+	}
+		FallBack "Diffuse"
+}
