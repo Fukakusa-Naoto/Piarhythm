0baf083f86de559c7c6d5c2253d864f127434e08
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-08-06 14:30:38 +0900

    設定シーンの実装完了（前のシーンに戻る機能は未実装）

--- Piarhythm/Assets/Scripts/Setting/SettingManager.cs
+++ Piarhythm/Assets/Scripts/Setting/SettingManager.cs
@@ -21,10 +21,12 @@ using UnityEngine.UI;
 public struct SystemData
 {
 	// ノーツの流れる速度
-	public float speed { get; set; }
+	public float speed;
 	// 鍵盤の数
-	public int keyNumber { get; set; }
+	public int keyNumber;
 }
+
+
 public class SettingManager : MonoBehaviour
 {
 	// <メンバ変数>
@@ -43,15 +45,15 @@ public class SettingManager : MonoBehaviour
 	//-----------------------------------------------------------------
 	private void Start()
 	{
-		string dataFilePass = UnityEngine.Application.dataPath + "/Resources/Data/System/SystemData.json";
+		string dataFilePath = UnityEngine.Application.dataPath + "/Resources/Data/System/SystemData.json";
 		m_systemData = new SystemData();
 
 		// ファイルの有無を調べる
-		if (File.Exists(dataFilePass))
+		if (File.Exists(dataFilePath))
 		{
 			// ファイルを読み込む
-			string json = File.ReadAllText(dataFilePass);
-			JsonUtility.FromJsonOverwrite(json, m_systemData);
+			string json = File.ReadAllText(dataFilePath);
+			m_systemData = JsonUtility.FromJson<SystemData>(json); ;
 		}
 		else
 		{
@@ -68,7 +70,7 @@ public class SettingManager : MonoBehaviour
 
 
 	//-----------------------------------------------------------------
-	//! @summary   ノーツ速度の上昇
+	//! @summary   ノーツ速度を上げる
 	//!
 	//! @parameter [void] なし
 	//!
@@ -86,4 +88,92 @@ public class SettingManager : MonoBehaviour
 
 
 
+	//-----------------------------------------------------------------
+	//! @summary   ノーツ速度を下げる
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	public void DownSpeed()
+	{
+		// スピードを下げる
+		m_systemData.speed -= 0.1f;
+		// 最大最小を超えないように制限をかける
+		m_systemData.speed = Mathf.Clamp(m_systemData.speed, 1.0f, 10.0f);
+		// UIに反映する
+		m_speedText.text = m_systemData.speed.ToString("F1");
+	}
+
+
+
+	//-----------------------------------------------------------------
+	//! @summary   鍵盤数を上げる
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	public void UpKeyNumber()
+	{
+		// 鍵盤数を上げる
+		switch(m_systemData.keyNumber)
+		{
+			case 88: break;
+			case 76: m_systemData.keyNumber = 88; break;
+			case 61: m_systemData.keyNumber = 76; break;
+			case 44: m_systemData.keyNumber = 61; break;
+			default: m_systemData.keyNumber = 61; break;
+		}
+
+		// テキストに反映させる
+		m_keyText.text = m_systemData.keyNumber.ToString() + "鍵盤";
+	}
+
+
+
+	//-----------------------------------------------------------------
+	//! @summary   鍵盤数を下げる
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	public void DownKeyNumber()
+	{
+		// 鍵盤数を下げる
+		switch(m_systemData.keyNumber)
+		{
+			case 88: m_systemData.keyNumber = 76; break;
+			case 76: m_systemData.keyNumber = 61; break;
+			case 61: m_systemData.keyNumber = 44; break;
+			case 44: break;
+			default: m_systemData.keyNumber = 44; break;
+		}
+
+		// テキストに反映させる
+		m_keyText.text = m_systemData.keyNumber.ToString() + "鍵盤";
+	}
+
+
+
+	//-----------------------------------------------------------------
+	//! @summary   設定シーンを閉じる
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	public void RevertSettingScene()
+	{
+		// json文字列に変換する
+		string json = JsonUtility.ToJson(m_systemData);
+
+		// ファイルに書き出し
+		string dataFilePath = UnityEngine.Application.dataPath + "/Resources/Data/System/SystemData.json";
+		File.WriteAllText(dataFilePath, json);
+
+		// 前のシーンに戻る
+
+	}
 }
