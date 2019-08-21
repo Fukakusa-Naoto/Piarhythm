2c11b9967baccfd6b6fe28990b911f8d37ebbbfc
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-08-21 11:43:31 +0900

    BGMの開始時間と終了時間の実装完了

--- Piarhythm/Assets/Scripts/Edit/EditManager.cs
+++ Piarhythm/Assets/Scripts/Edit/EditManager.cs
@@ -20,6 +20,18 @@ using UnityEngine.SceneManagement;
 
 
 // クラスの定義 =============================================================
+[System.Serializable]
+public struct BGMData
+{
+	// ファイルパス
+	public string path;
+	// 開始時間
+	public float startTime;
+	// 終了時間
+	public float endTime;
+}
+
+
 public class EditManager : MonoBehaviour
 {
 	// <メンバ変数>
@@ -32,7 +44,10 @@ public class EditManager : MonoBehaviour
 	public Transform[] m_layers;
 	public MusicalScoreController m_musicalScore;
 	private SystemData m_systemData;
-
+	public GameObject m_startInputField;
+	public GameObject m_endInputField;
+	public Text m_bgmText;
+	private BGMData m_bgmData;
 
 
 	// メンバ関数の定義 =====================================================
@@ -45,6 +60,7 @@ public class EditManager : MonoBehaviour
 	//-----------------------------------------------------------------
 	void Start()
 	{
+		m_bgmData = new BGMData();
 		m_audioSource = gameObject.GetComponent<AudioSource>();
 
 		string dataFilePath = UnityEngine.Application.dataPath + "/Resources/Data/System/SystemData.json";
@@ -80,7 +96,11 @@ public class EditManager : MonoBehaviour
 		if (m_audioClip)
 		{
 			// 再生終了
-			if (Mathf.Approximately(m_audioSource.time, m_audioClip.length)) m_musicalScore.Stop();
+			if (m_audioSource.time >= m_bgmData.endTime)
+			{
+				m_audioSource.Stop();
+				m_musicalScore.Stop();
+			}
 			m_nowTime.text = m_audioSource.time.ToString();
 		}
 
@@ -104,10 +124,8 @@ public class EditManager : MonoBehaviour
 	//-----------------------------------------------------------------
 	public void OpenExistFile()
 	{
-
 		OpenFileDialog open_file_dialog = new OpenFileDialog
 		{
-
 			//ファイルが実在しない場合は警告を出す(true)、警告を出さない(false)
 			CheckFileExists = false
 		};
@@ -134,11 +152,17 @@ public class EditManager : MonoBehaviour
 	{
 		var www = UnityWebRequestMultimedia.GetAudioClip("file://" + file, AudioType.WAV);
 		yield return www.SendWebRequest();
+
+		// 読み込み完了
 		m_audioClip = m_audioSource.clip = DownloadHandlerAudioClip.GetContent(www);
 		string[] str = file.Split('\\');
-		m_audioClip.name = str[str.Length - 1];
+		m_bgmText.text = m_audioClip.name = str[str.Length - 1];
 		m_maxTime.text = "/\t";
 		m_maxTime.text += m_audioClip.length.ToString();
+		Text text = m_endInputField.transform.GetChild(1).GetComponent<Text>();
+		text.text = m_audioClip.length.ToString();
+		m_bgmData.startTime = 0.0f;
+		m_bgmData.endTime = m_audioClip.length;
 	}
 
 
@@ -152,6 +176,7 @@ public class EditManager : MonoBehaviour
 	//-----------------------------------------------------------------
 	public void OnPlayButton()
 	{
+		m_audioSource.time = m_bgmData.startTime;
 		if (!m_audioSource.isPlaying) m_audioSource.Play();
 		m_musicalScore.Play();
 	}
@@ -241,5 +266,37 @@ public class EditManager : MonoBehaviour
 	public float GetNotesSpeed()
 	{
 		return m_systemData.speed;
+	}
+
+
+
+	//-----------------------------------------------------------------
+	//! @summary   開始時間の取得
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	public void OnStartTimeInputField()
+	{
+		// 文字列を数値に変換してデータ構造体に保管する
+		InputField inputField = m_startInputField.GetComponent<InputField>();
+		m_bgmData.startTime = float.Parse(inputField.text);
+	}
+
+
+
+	//-----------------------------------------------------------------
+	//! @summary   終了時間の取得
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	public void OnEndTimeInputField()
+	{
+		// 文字列を数値に変換してデータ構造体に保管する
+		InputField inputField = m_endInputField.GetComponent<InputField>();
+		m_bgmData.endTime = float.Parse(inputField.text);
 	}
 }
