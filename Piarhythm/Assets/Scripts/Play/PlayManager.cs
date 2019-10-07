5d05709367ab7e8c31329700c50cc82d61f5941a
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-10-07 17:30:34 +0900

    音楽ファイルの読み込み処理の変更

--- Piarhythm/Assets/Scripts/Play/PlayManager.cs
+++ Piarhythm/Assets/Scripts/Play/PlayManager.cs
@@ -23,6 +23,7 @@ public class PlayManager : MonoBehaviour
 	// <メンバ変数>
 	private AudioSource m_audioSource;
 	private AudioClip m_audioClip = null;
+	private MusicPieceData m_musicPieceData;
 
 
 	// メンバ関数の定義 =====================================================
@@ -37,10 +38,21 @@ public class PlayManager : MonoBehaviour
 	{
 		m_audioSource = GetComponent<AudioSource>();
 
-		// コルーチンのスタート
+		// セーブデータから読み込む楽曲ファイルパスを取得する
+		string filePath = PlayerPrefs.GetString("Path", "");
+
+		// 楽曲データを読み込む
+		string jsonStr = "";
+		StreamReader reader;
+		reader = new StreamReader(filePath);
+		jsonStr = reader.ReadToEnd();
+		reader.Close();
+		m_musicPieceData = JsonUtility.FromJson<MusicPieceData>(jsonStr);
+
+		// BGMの読み込みコルーチンのスタート
 		if (m_audioClip == null)
-			StartCoroutine("Load",
-				UnityEngine.Application.dataPath + "/Resources/BGM/YUBIKIRI-GENMAN -special edit-.wav");
+			//StartCoroutine("Load", m_musicPieceData.bgmData.path);
+			StartCoroutine(LoadToAudioClip(m_musicPieceData.bgmData.path));
 	}
 
 
@@ -54,7 +66,7 @@ public class PlayManager : MonoBehaviour
 	//-----------------------------------------------------------------
 	void Update()
 	{
-		if (Input.GetKey(KeyCode.Space)) m_audioSource.Play();
+		if ((m_audioClip != null) && (!m_audioSource.isPlaying)) m_audioSource.Play();
 	}
 
 
@@ -69,10 +81,56 @@ public class PlayManager : MonoBehaviour
 	IEnumerator Load(string file)
 	{
 		//AudioType.MPEG
-		var www = UnityWebRequestMultimedia.GetAudioClip("file://" + file, AudioType.WAV);
-		yield return www.SendWebRequest();
-		m_audioClip = m_audioSource.clip = DownloadHandlerAudioClip.GetContent(www);
-		string[] str = file.Split('\\');
-		m_audioClip.name = str[str.Length - 1];
+		using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("file://" + file, AudioType.WAV))
+		{
+			yield return www.SendWebRequest();
+
+			if (www.isNetworkError)
+			{
+				Debug.Log(www.error);
+			}
+			else
+			{
+				m_audioClip = m_audioSource.clip = DownloadHandlerAudioClip.GetContent(www);
+				string[] str = file.Split('\\');
+				m_audioClip.name = str[str.Length - 1];
+				m_audioSource.time = m_musicPieceData.bgmData.startTime;
+			}
+		}
+	}
+
+
+
+	IEnumerator LoadToAudioClip(string path)
+	{
+		if (m_audioSource == null || string.IsNullOrEmpty(path))
+			yield break;
+
+		if (!File.Exists(path))
+		{
+			//ここにファイルが見つからない処理
+			Debug.Log("File not found.");
+			yield break;
+		}
+
+		using (WWW www = new WWW("file://" + path))  //※あくまでローカルファイルとする
+		{
+			while (!www.isDone)
+				yield return null;
+
+			AudioClip audioClip = www.GetAudioClip(false, true);
+			if (audioClip.loadState != AudioDataLoadState.Loaded)
+			{
+				//ここにロード失敗処理
+				Debug.Log("Failed to load AudioClip.");
+				yield break;
+			}
+
+			//ここにロード成功処理
+			m_audioClip = m_audioSource.clip = audioClip;
+			string[] str = path.Split('\\');
+			m_audioClip.name = str[str.Length - 1];
+			m_audioSource.time = m_musicPieceData.bgmData.startTime;
+		}
 	}
 }
