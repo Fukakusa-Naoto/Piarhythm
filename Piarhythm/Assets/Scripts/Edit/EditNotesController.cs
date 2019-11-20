958690a709c994d9da6474b741e11177f7f74f5d
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-11-20 14:55:54 +0900

    キーの位置が分かりやすい背景にした

--- Piarhythm/Assets/Scripts/Edit/EditNotesController.cs
+++ Piarhythm/Assets/Scripts/Edit/EditNotesController.cs
@@ -281,7 +281,7 @@ public class EditNotesController : MonoBehaviour
 		//m_transform.sizeDelta = new Vector2(width, m_transform.sizeDelta.y);
 
 		Vector3 localScale = m_transform.localScale;
-		localScale.x = (scale.Contains("#")) ? 0.75f : 1.0f;
+		localScale.x = (scale.Contains("#")) ? 0.65f : 0.9f;
 		m_transform.localScale = localScale;
 
 		// #の色を変化させる
