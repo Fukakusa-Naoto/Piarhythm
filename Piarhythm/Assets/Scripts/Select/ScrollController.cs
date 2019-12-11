4ba8c6f418b5039a4e4b356cea93aad06ab9d905
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-12-11 09:18:18 +0900

    楽曲選択シーンのレイアウトを変更

--- Piarhythm/Assets/Scripts/Select/ScrollController.cs
+++ Piarhythm/Assets/Scripts/Select/ScrollController.cs
@@ -13,6 +13,7 @@ using UnityEngine;
 using System;
 using System.Collections;
 using UnityEngine.UI;
+using System.IO;
 
 
 // クラスの定義 =============================================================
@@ -20,36 +21,12 @@ public class ScrollController : MonoBehaviour
 {
 	// <メンバ変数>
 	// 楽曲タイルのプレハブ
-	public GameObject m_soundTilePrefab;
-	// 分割数
-	private int m_divNum = 20;
-	// 半径の距離
-	private float m_range = 400.0f;
-
-	// クリックされた時の最初のX座標
-	private float m_startXPosition;
-	// クリックされている間のX座標
-	private float m_nowXPosition;
-	// リックされた時の最初の角度
-	private Vector3 m_startRotation;
-
-	private int m_currentTileNumber;
-	private int m_nextTileNumber;
-
-	// タイルの順番を記憶しておく (0:左,4:右)
-	private Transform[] m_soundTiles;
-
-	// 正面のタイルの番号
-	private int m_frontTileNumber;
-
-	// 楽曲名リスト
-	private string[] m_musicPieceList;
-
-	// 選択シーンマネージャー
-	public SelectManager m_selectManager;
+	[SerializeField]
+	private GameObject m_soundTilePrefab = null;
 
 
 	// メンバ関数の定義 =====================================================
+	#region 初期化処理
 	//-----------------------------------------------------------------
 	//! @summary   初期化処理
 	//!
@@ -59,50 +36,23 @@ public class ScrollController : MonoBehaviour
 	//-----------------------------------------------------------------
 	void Start()
     {
-		for(int i = 0; i < 5; ++i)
+		// 楽曲データを取得する
+		string[] musicPieceArray = Directory.GetFiles(PiarhythmDatas.MUSIC_PIECE_DIRECTORY_PATH, "*.json", SearchOption.AllDirectories);
+
+		foreach(string musicPiece in musicPieceArray)
 		{
-			// 角度を求める
-			float deg = (360 / m_divNum) * i;
-			deg += (360 / m_divNum) * 13;
-			float rad = deg * Mathf.Deg2Rad;
-
-			// 座標を求める
-			float x = m_range * Mathf.Cos(rad);
-			float z = m_range * Mathf.Sin(rad);
-
-			// タイルを生成する
+			// タイルを作成する
 			GameObject tile = Instantiate(m_soundTilePrefab);
 
-			// タイルを配置する
-			RectTransform rectTransform = tile.GetComponent<RectTransform>();
-			rectTransform.position = new Vector3(x, 0.0f, z) + transform.position;
+			// テキストを更新する
+			string musicName = Path.GetFileNameWithoutExtension(musicPiece);
+			tile.GetComponent<SoundTileController>().SetMusicName(musicName);
 
-			// 子に設定する
-			rectTransform.parent = transform;
-		}
-
-		m_currentTileNumber = m_nextTileNumber = 0;
-
-		// タイルの並び順を記憶しておく
-		m_soundTiles = new Transform[transform.childCount];
-		for (int i = 0; i < transform.childCount; ++i) m_soundTiles[i] = transform.GetChild(i);
-
-
-		// Z座標順に描画を変更する
-		ZSort();
-
-		m_frontTileNumber = 0;
-
-		m_musicPieceList = m_selectManager.GetMusicPieceList();
-
-		// タイルに曲名を入れていく
-		for (int i = 0; i < m_soundTiles.Length; ++i)
-		{
-			m_soundTiles[i].GetChild(0).GetComponent<Text>().text =
-				m_musicPieceList[(m_musicPieceList.Length - i + m_frontTileNumber - 2) % m_musicPieceList.Length];
+			// 親子関係を組む
+			tile.transform.SetParent(transform);
 		}
 	}
-
+	#endregion
 
 
 	//-----------------------------------------------------------------
@@ -114,217 +64,5 @@ public class ScrollController : MonoBehaviour
 	//-----------------------------------------------------------------
 	void Update()
     {
-		// 左クリックがされた
-		if(Input.GetMouseButtonDown(0))
-		{
-			// 最初の座標と角度を記録
-			m_startXPosition = Input.mousePosition.x;
-			m_startRotation = transform.rotation.eulerAngles;
-			m_currentTileNumber = 0;
-		}
-		// マウスの左ボタンが押されている
-		else if(Input.GetMouseButton(0))
-		{
-			// 現在の座標を更新する
-			m_nowXPosition = Input.mousePosition.x;
-
-			// 移動量を計算する
-			float movement = m_nowXPosition - m_startXPosition;
-			movement *= 0.1f;
-
-			// 新しい角度を作成して代入する
-			m_nextTileNumber = (int)(movement / (360 / m_divNum));
-
-
-			if (m_currentTileNumber < m_nextTileNumber)		// 右回転
-			{
-				// 一番左のタイルの座標を記録する
-				Vector3 tmpPosition = m_soundTiles[0].GetComponent<RectTransform>().position;
-
-				// 1フレームのタイルの移動量に制限をかける
-				m_nextTileNumber = m_currentTileNumber + Mathf.Clamp(m_nextTileNumber - m_currentTileNumber, 0, 1);
-
-				// 正面のタイルを更新する
-				m_frontTileNumber += m_nextTileNumber;
-				if (m_frontTileNumber < 0) m_frontTileNumber = m_musicPieceList.Length - 1;
-				else if (m_frontTileNumber > m_musicPieceList.Length - 1) m_frontTileNumber = 0;
-
-				// 全体を回転させる
-				Vector3 newRotation = new Vector3(0.0f, -m_nextTileNumber * (360 / m_divNum), 0.0f);
-				transform.rotation = Quaternion.Euler(newRotation + m_startRotation);
-
-				// 正面のタイルの値を更新する
-				m_currentTileNumber = m_nextTileNumber;
-
-				// 一番右のタイルを一番左に移動させる
-				m_soundTiles[m_soundTiles.Length - 1].GetComponent<RectTransform>().position = tmpPosition;
-
-				// タイルの順番を右にシフトさせる
-				ArrayRotate(ref m_soundTiles, 1);
-
-				// 描画順を調整する
-				ZSort();
-
-				// タイルに曲名を入れていく
-				for (int i = 0; i < m_soundTiles.Length; ++i)
-				{
-					m_soundTiles[i].GetChild(0).GetComponent<Text>().text =
-						m_musicPieceList[(m_musicPieceList.Length - i + m_frontTileNumber - 2) % m_musicPieceList.Length];
-				}
-			}
-			else if(m_currentTileNumber > m_nextTileNumber)		// 左回転
-			{
-				// 一番右のタイルの座標を記録する
-				Vector3 tmpPosition = m_soundTiles[m_soundTiles.Length - 1].GetComponent<RectTransform>().position;
-
-				// 1フレームのタイルの移動量に制限をかける
-				m_nextTileNumber = m_currentTileNumber + Mathf.Clamp(m_nextTileNumber - m_currentTileNumber, -1, 0);
-
-				// 正面のタイルを更新する
-				m_frontTileNumber += m_nextTileNumber;
-				if (m_frontTileNumber < 0) m_frontTileNumber = m_musicPieceList.Length - 1;
-				else if (m_frontTileNumber > m_musicPieceList.Length - 1) m_frontTileNumber = 0;
-
-				// 全体を回転させる
-				Vector3 newRotation = new Vector3(0.0f, -m_nextTileNumber * (360 / m_divNum), 0.0f);
-				transform.rotation = Quaternion.Euler(newRotation + m_startRotation);
-
-				// 正面のタイルの値を更新する
-				m_currentTileNumber = m_nextTileNumber;
-
-				// 一番左のタイルを一番右に移動させる
-				m_soundTiles[0].GetComponent<RectTransform>().position = tmpPosition;
-
-				// タイルの順番を左にシフトさせる
-				ArrayRotate(ref m_soundTiles, -1);
-
-				// 描画順を調整する
-				ZSort();
-
-				// タイルに曲名を入れていく
-				for (int i = 0; i < m_soundTiles.Length; ++i)
-				{
-					m_soundTiles[i].GetChild(0).GetComponent<Text>().text =
-						m_musicPieceList[(m_musicPieceList.Length - i + m_frontTileNumber - 2) % m_musicPieceList.Length];
-				}
-			}
-		}
-		else if(Input.GetMouseButtonUp(0))
-		{
-			if(m_startXPosition.Equals(m_nowXPosition))
-			{
-				// 楽曲が選択された
-				m_selectManager.OnSelectEnter(m_frontTileNumber);
-			}
-		}
-	}
-
-
-
-	//-----------------------------------------------------------------
-	//! @summary   Z座標でソートする
-	//!
-	//! @parameter [void] なし
-	//!
-	//! @return    なし
-	//-----------------------------------------------------------------
-	private void ZSort()
-	{
-		// 子オブジェクトを取得する
-		Transform[] children = new Transform[transform.childCount];
-		for (int i = 0; i < transform.childCount; ++i) children[i] = transform.GetChild(i);
-
-		QuickSort(children, 0, children.Length - 1);
-
-		for (int i = 0; i < transform.childCount; ++i) children[i].SetAsFirstSibling();
-	}
-
-
-
-	/*
-   * 軸要素の選択
-   * 順に見て、最初に見つかった異なる2つの要素のうち、
-   * 大きいほうの番号を返します。
-   * 全部同じ要素の場合は -1 を返します。
-   */
-	int Pivot(Transform[] a, int i, int j)
-	{
-		int k = i + 1;
-		while (k <= j && a[i].position.z.Equals(a[k].position.z)) k++;
-		if (k > j) return -1;
-		if (a[i].position.z >= a[k].position.z) return i;
-		return k;
-	}
-
-	/*
-	 * パーティション分割
-	 * a[i]～a[j]の間で、x を軸として分割します。
-	 * x より小さい要素は前に、大きい要素はうしろに来ます。
-	 * 大きい要素の開始番号を返します。
-	 */
-	int Partition(Transform[] a, int i, int j, Transform x)
-	{
-		int l = i, r = j;
-
-		// 検索が交差するまで繰り返します
-		while (l <= r)
-		{
-			// 軸要素以上のデータを探します
-			while (l <= j && a[l].position.z < x.position.z) l++;
-
-			// 軸要素未満のデータを探します
-			while (r >= i && a[r].position.z >= x.position.z) r--;
-
-			if (l > r) break;
-			Swap(ref a[l], ref a[r]);
-			l++; r--;
-		}
-		return l;
-	}
-
-	/*
-	 * クイックソート（再帰用）
-	 * 配列aの、a[i]からa[j]を並べ替えます。
-	 */
-	public void QuickSort(Transform[] a, int i, int j)
-	{
-		if (i == j) return;
-		int p = Pivot(a, i, j);
-		if (p != -1)
-		{
-			int k = Partition(a, i, j, a[p]);
-			QuickSort(a, i, k - 1);
-			QuickSort(a, k, j);
-		}
-	}
-
-
-	static void Swap<T>(ref T lhs, ref T rhs)
-	{
-		T temp;
-		temp = lhs;
-		lhs = rhs;
-		rhs = temp;
-	}
-
-
-	//-----------------------------------------------------------------
-	//! @summary   配列をローテーションさせる
-	//!
-	//! @parameter [array] ローテーションさせる配列
-	//! @parameter [shiftCount] シフトさせる数
-	//!
-	//! @return    なし
-	//-----------------------------------------------------------------
-	public static void ArrayRotate<T>(ref T[] array, int shiftCount)
-	{
-		T[] backupArray = new T[array.Length];
-
-		for (int i = 0; i < array.Length; i++)
-		{
-			backupArray[(i + array.Length + shiftCount % array.Length) % array.Length] = array[i];
-		}
-
-		array = backupArray;
 	}
 }
--- Piarhythm/Assets/Scripts/Select/SoundTileController.cs
+++ Piarhythm/Assets/Scripts/Select/SoundTileController.cs
@@ -1,25 +1,69 @@
-﻿using System.Collections;
+﻿//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+//! @file		SoundTileController.cs
+//!
+//! @summary	楽曲タイルの制御に関するC#スクリプト
+//!
+//! @date		2019.12.10
+//!
+//! @author		深草直斗
+//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+
+// 名前空間の省略 ===========================================================
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+// クラスの定義 =============================================================
 public class SoundTileController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+	// <メンバ変数>
+	// 曲名
+	private string m_musicName = null;
 
+	// コンポーネント
+	private RectTransform m_transform = null;
+	private Text m_text = null;
+
+
+	// メンバ関数の定義 =====================================================
+	#region 初期化処理
+	//-----------------------------------------------------------------
+	//! @summary   初期化処理
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	private void Start()
+    {
+		// コンポーネントの取得
+		m_text = transform.GetChild(0).GetComponent<Text>();
+		m_transform = GetComponent<RectTransform>();
+
+		// テキストを更新
+		m_text.text = m_musicName;
+
+		// 親子関係を組んだ時のずれを修正する
+		m_transform.localScale = Vector3.one;
+		Vector3 position = m_transform.localPosition;
+		position.z = 0.0f;
+		m_transform.localPosition = position;
     }
+	#endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-		DirectFront();
-	}
-
-
-	public void DirectFront()
+	#region 曲名の設定する
+	//-----------------------------------------------------------------
+	//! @summary   曲名の設定する
+	//!
+	//! @parameter [musicName] 設定する曲名
+	//!
+	//! @return    なし
+	//-----------------------------------------------------------------
+	public void SetMusicName(string musicName)
 	{
-		Vector3 euler = transform.parent.GetComponent<RectTransform>().rotation.eulerAngles;
-		GetComponent<RectTransform>().localRotation = Quaternion.Euler(new Vector3(0.0f, -euler.y, 0.0f));
+		m_musicName = musicName;
 	}
+	#endregion
 }
